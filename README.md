df6584a68645f1daf26f9bade69238ed622a60c2
Andrew Martin <andy123@stanford.edu>
2020-03-27 16:18:58 -0700

    added field to track summary instance

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # Summarize Instance
-An EM that allows you to save a single instance of a repeating form (or repeating event) in another event.
-This can be useful to keep the 'latest' version of a something repeating easily accessible for piping and reporting.
+An EM that allows you to save a single instance of a repeating form in another event.
 
+When the repeating form is saved, it can be compared against the singleton form in the other event to decide if the singleton
+form should be replaced with the data from the current form.
 
-## Project Setup
+### Project Setup
+ * Start with an event with a repeating form (e.g. medications in the baseline event)
+ * Enable this form in another event as a non-repeating form (e.g. medications in most_recent event)
 
-##Configuration options
+### EM Configuration
+Configure the EM by specifying the:
+ * [source-event-id]  (e.g. baseline_arm_1)
+ * [source-form]      (e.g. medications)
+ * [logic] (optional, e.g. [visit_date] > [most_recent_arm_1][visit_date])
+   * Note how the first visit date is not prepended with an event_name.  This means it will be executed in the context of
+   the current event in which it was saved.  The same applies to instance number.
+ * [destination-event-id] (e.g. recent_arm_1)
+
+It is possible to configure many summary instances in a single project
+
+#### TODO Enhancements
+- make it so that it also supports repeating events, not just repeating forms
+- think about making it so you can evaluate logic across all instances... How would this work?  Imagine the summary instance
+was the most recent visit detail.  If you deleted an instance, you would have to evaluate all of the instances to find the one
+with the most recent visit_date...  Not sure how to approach this - it would require custom logic handling...  It could,
+for example, sort all instances by a field and take the first or last of them.
 
--- SummarizeInstance.php
+++ SummarizeInstance.php
@@ -3,6 +3,7 @@
 namespace Stanford\SummarizeInstance;
 
 require_once("emLoggerTrait.php");
+require_once("RepeatingForms.php");
 
 use \REDCap;
 
@@ -31,13 +32,22 @@ class SummarizeInstance extends \ExternalModules\AbstractExternalModule
         // Loop over all instances
         foreach ($instances as $i => $instance) {
             // Get fields from current instance
-            $source_event_id      = $instance['source-event-id'];
-            $source_form          = $instance['source-form'];
-            $logic                = $instance['logic'];
-            $destination_event_id = $instance['destination-event-id'];
+            $source_event_id           = $instance['source-event-id'];
+            $source_form               = $instance['source-form'];
+            $logic                     = $instance['logic'];
+            $destination_event_id      = $instance['destination-event-id'];
+            $destination_summary_field = $instance['destination-summary-field'];
+
+            $event_names = REDCap::getEventNames(true,true);
+            $event_name = $event_names[$event_id];
 
             if ($event_id !== $source_event_id) {
                 $this->emDebug("Skipping event $event_id");
+                continue;
+            }
+
+            if ($instrument !== $instance['source-form']) {
+                $this->emDebug("Skipping form $instrument");
                 continue;
             }
 
@@ -45,8 +55,55 @@ class SummarizeInstance extends \ExternalModules\AbstractExternalModule
             $rp = new RepeatingForms($project_id, $instrument);
 
             if($rp === false) {
-                $this->emLog($rp->last_error_message);
+                $this->emLog("Unable to instantiate RepeatingForms", $rp->last_error_message);
             }
+
+            // Determine if we should do the summary copy
+            if (!empty($logic)) {
+                $result = \REDCap::evaluateLogic($logic,$project_id,$record,$event_name,$repeat_instance, $instrument);
+                $this->emDebug("Logic Evaluated:  $logic => " . ($result ? 'True' : 'False'));
+                if (!$result) {
+                    // Failed logic - skip
+                    continue;
+                }
+            }
+
+            // Make sure form is enabled in destination event id
+            global $Proj;
+            // $this->emDebug($Proj->eventsForms[$destination_event_id]);
+
+            if (! in_array($instrument, $Proj->eventsForms[$destination_event_id])) {
+                $this->emLog("$instrument is not defined in the destination event id");
+                continue;
+            }
+
+            // Load the data
+            $data = $rp->getInstanceById( $record, $repeat_instance, $event_id);
+            $data['redcap_event_name'] = $event_names[$destination_event_id];
+            $data[\REDCap::getRecordIdField()] = $record;
+
+            if (!empty($destination_summary_field)) {
+                // Verify field exists in destination event
+                $dest_fields = \REDCap::getValidFieldsByEvents($project_id,$destination_event_id,false);
+                //$this->emDebug($dest_fields);
+                if(!in_array($destination_summary_field, $dest_fields)) {
+                    $this->emError("Destination Summary Field $destination_summary_field is not enabled in destination event $destination_event_id");
+                } else {
+                    $data[$destination_summary_field] = $repeat_instance;
+                }
+            }
+
+
+            $result = REDCap::saveData('json', json_encode(array($data)), 'overwrite');
+            if (!empty($result['errors'])) {
+                $this->emError($result);
+                REDCap::logEvent("Summary Instance Update Failure","Failed to update summary instance for record $record instance $repeat_instance on form $instrument","", $record, $event_id, $project_id);
+            } else {
+                REDCap::logEvent("Summary Instance Update","","", $record, $event_id, $project_id);
+                $this->emDebug("Summary Instance Updated");
+            }
+
+            // $this->emDebug("DATA", $data, $result);
         }
 
     }
